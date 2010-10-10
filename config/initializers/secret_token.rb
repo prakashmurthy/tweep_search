c3779343a8107462509fc709bdaf5ba5c1f97a1c
Prakash Murthy <prakblr@yahoo.com>
2010-10-10 16:56:36 -0600

    run rails new tweep_search

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweepSearch::Application.config.secret_token = '796155364e6fbc286f808333b91e0e056867c1ce0bd3016d225a8086b0ea88128512ce641f075240b00efa3ae90f7004eacfc09ab96229788586232fff02bf25'
